2066a77b02bdd8953d7af026cc132868d7be03a9
unknown <ygomben2510@g.educaand.es>
2025-11-13 13:22:52 +0100

    Comienzo ej 1

--- Tema-06/Actividades/Validator.js
+++ Tema-06/Actividades/Validator.js
@@ -1,12 +1,23 @@
-/*class BajaCoche {
+class BajaCoche {
     constructor() {
-        this.#fEvento();
+        this.fEvento();
+    }
+
+    validaMatricula() {
+        var matricula = document.getElementById("matricula");
+        if (matricula.validity.typeMismatch) {
+            matricula.setCustomValidity("Formato matrícula erróneo");
+        } else {
+            matricula.setCustomValidity("");
+        }
+        matricula.reportValidity();
+
     }
 
     fEvento() {
-    }
-}*/
+        var matricula = document.getElementById("matricula");
+        matricula.addEventListener("focus", this.validaMatricula.bind(this));
 
-dniSelect() {
-    if ()
+    }
 }
+window.addEventListener("load",()=>new BajaCoche());
